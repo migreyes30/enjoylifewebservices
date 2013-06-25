d0fc7fb0f7f116e6d26f12a87499ce7cc5b546f4
jgordon87@gmail.com <jgordon87@gmail.com@e0aa1116-f73d-b37a-dc2b-603cd4de045d>
2013-06-25 05:25:26 +0000

    Mike es puto !!!

---
+++ trunk/config/variables.php
@@ -0,0 +1,19 @@
+<?php
+#cambiar colores en los links.
+
+#IP del servidor
+
+$IPSERVER = $_SERVER['HTTP_HOST'];
+
+#Base de datos
+
+$DATABASE_NAME = 'enjoylife';
+$DATABASE_HOSTNAME = 'localhost';
+$DATABASE_PORT     = 27017;
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'password';
+
+$PASSPHRASE = 'mexico00';
+
+
+?>
