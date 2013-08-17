883c766608640564df084c16a794d75133038ef1
migue030@gmail.com <migue030@gmail.com@e0aa1116-f73d-b37a-dc2b-603cd4de045d>
2013-08-17 19:58:55 +0000

    Correct sintax

--- config/variables.php
+++ config/variables.php
@@ -6,21 +6,20 @@
 $IPSERVER = $_SERVER['HTTP_HOST'];
 
 #Base de datos
+$DATABASE_NAME = 'enjoylife';
 
 
-/*
---Localost
-$DATABASE_HOSTNAME = 'localhost';
+#--Localost
+#$DATABASE_HOSTNAME = 'localhost';
 
---Ip Gordon
+#--Ip Gordon
+#$DATABASE_HOSTNAME = '192.168.1.14';
+
+#--Servidor Amazon
+#$DATABASE_HOSTNAME = '54.235.245.127';
+
 $DATABASE_HOSTNAME = '192.168.1.14';
 
---Servidor Amazon
-$DATABASE_HOSTNAME = '54.235.245.127';
-*/
-
-$DATABASE_NAME = 'enjoylife';
-$DATABASE_HOSTNAME = 'localhost';
 $DATABASE_PORT     = 27017;
 $DATABASE_USER = 'root';
 $DATABASE_PASS = 'password';
