7b7c27bc17ef00d226dae006c573e8c42f33e2ec
jgordon87@gmail.com <jgordon87@gmail.com@e0aa1116-f73d-b37a-dc2b-603cd4de045d>
2014-03-12 02:57:52 +0000

    cambios al servicio de cambiar password

--- controllers/setNewPassword.php
+++ controllers/setNewPassword.php
@@ -29,7 +29,7 @@
 
         $result = $clientes->find($query,$isActive);
         $pwdInDB = "";
-        $userInDB = "":
+        $userInDB = "";
         $array = iterator_to_array($result);
 
     	foreach ($result as $obj) {
@@ -48,8 +48,38 @@
 				); 
 
         	//sendRecoverPasswordEmail($usuario,$email,$name);
+        }else{
+        	header("location:../clients/recuperaPassword.php?");
         }
 
         $mongoDB->close();
 
 ?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Enjoy Health - Password actualizado</title>
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8" /> 
+	<meta name="Keywords" content="Enjoy Health"/>
+	<link rel="shortcut icon" href="images/favicon.ico" />
+	<link href="../css/style.css" rel="stylesheet" type="text/css" media="screen"/>
+    <script type="text/javascript" src="../js/jquery-1.8.0.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.validate.min.js"> </script>	
+
+</head>
+<body>
+	<div id="container">
+		<div id="contenido" style="height:630px;">
+			<h1>
+				Estimado <?php echo $name; ?>
+			</h1>
+			<span> Tu password ha sido cambiado, ahora podrás logearte con tu nuevo password en la aplicación. </span>		
+
+		</div>
+
+	</div>
+
+</body>
+</html>
